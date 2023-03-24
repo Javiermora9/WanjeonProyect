63f3e589fcc6ed8bde8df0318a1152820de416c9
Javiermora9 <francisco.mora.2018@upb.edu.co>
2023-03-24 17:52:58 -0500

    mas cambios

---
+++ Proyecto Wanjeon/Productos.js
@@ -0,0 +1,31 @@
+const productos = ()=>[
+
+    {
+        "id": 1,
+        "title": "Aceite esencial de Clavo",
+        "amount": "12ML.",
+        "price": 7.99,
+        "description": "El aceite esencial de clavo es conocido por sus increíbles propiedades antimicrobianas, antimicóticas, antisépticas, antivirales, afrodisíacas y estimulantes. Perfecto para utilizar en tus mezclas de Cosmética Natural, añadiendo unas cuantas gotas en tu crema corporal o aceite vegetal, conseguirás nutrir y lucir una piel radiante y 100% cuidad.",
+
+        
+    },
+    {
+
+        "id": 2,
+        "title": "Parches de Oro de 24 kt Rejuvenecedores para Contorno de Ojos",
+        "amount": "60UDS.",
+        "price": 15.50,
+        "description": "Sale a: 0,26 €/ud.\nParches de oro de 24 kt rejuvenecedores para contorno de ojos de Natura Siberica. Parches para ojos con efecto rejuvenecedor enriquecidos con oro de 24kt. Su acción descongestiona la piel, la suaviza y mejora su luminosidad. Estos parches con oro de 24kt son la opción perfecta para rejuvenecer la mirada en pocos minutos. Basados en activo postbiótico único y extractos fermentados de mora de los pantanos norteña y frambuesa silvestre, contienen dos activos únicos para combatir los signos de envejecimiento: el complejo de polipéptidos SYN-COLL® y oro de 24kt. Gracias a ellos, estimulan la síntesis de colágeno en las células de la piel y ayudan a suavizar las arrugas.",
+
+        
+
+        
+
+    }
+
+    
+
+
+
+
+]
--- Proyecto Wanjeon/Scripts.js
+++ Proyecto Wanjeon/Scripts.js
@@ -1,27 +1,122 @@
-const productos= ()=>[
+import productos from "./Productos.js"
+const products = productos();
 
-  {
-    "id": 1,
-    "title": "Crema nivea",
+
+let currentIndex = 0;
+
+function renderProduct() {
+    const product = products[currentIndex];
+
     
-},
+    const Cardproducts = `
 
-]
-const producto= productos();
+  <div class=" Contenedero"> 
 
 
-const productsCard =`
-
-<div class="card" style="">
-                <img src="" class="card-img-top" alt="...">
+        <div class="card" ">
+                <img src="src-eco-store/${product.id}.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">${products[0].title}/h5>
-                  <p class="card-text"></p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                
+                <button type="button" class="btn btn-danger"></button>
+                <button type="button" class="btn btn-success"></button>
+                <button type="button" class="btn btn-primary"></button>  
+                  
                 </div>
-              </div>
+        </div>
 
-`
 
-const cardP=document.getElementById("cardP");
-cardP.innerHTML = productsCard;
+ 
+    <div class="container ">
+      <div class="row">
+        <div class="col">
+          <div class="ID">
+            <label for="formGroupExampleInput" class="form-label">ID</label>
+            <input type="text" class="form-control" id="formGroupExampleInput_ID"  placeholder="${product.id}">
+          </div>
+          
+          <div class="mb">
+            <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="${product.description}"></textarea>
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Title</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="${product.title}">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="mb-2">
+            <label for="formGroupExampleInput" class="form-label">Precio</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="${product.price}">
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-2">
+            <label for="formGroupExampleInput" class="form-label">Cantidad</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="${product.amount}">
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-6">
+            <label for="formGroupExampleInput" class="form-label">Descuento</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="${product.discount}">
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-6">
+            <label for="formGroupExampleInput" class="form-label">%</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="${product.discountPer}">
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-6">
+            <label for="formGroupExampleInput" class="form-label">Unid.</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="${product.discountUni}">
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+
+</div>  
+  `;
+
+    const lugarProductos = document.getElementById('lugarProductos');
+    lugarProductos.innerHTML = Cardproducts;
+
+
+}
+
+function handleNextClick() {
+
+  if (currentIndex >= 26) {
+    currentIndex = 0;
+  }
+    currentIndex = (currentIndex + 1) % products.length;
+    renderProduct();
+}
+
+function handleNextClickleft() {
+
+  if (currentIndex <= 0) {
+    currentIndex = 26;
+  }
+
+    currentIndex = (currentIndex - 1) % products.length;
+    
+    renderProduct();
+}
+
+
+const btnNext = document.getElementById('btnNext');
+btnNext.addEventListener('click', handleNextClick);
+
+
+const btnleft = document.getElementById('btnleft');
+btnleft.addEventListener('click', handleNextClickleft);
+
+renderProduct();
