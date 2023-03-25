9f644c7a22c7eaaf1e6221a8b5d4d9dc68ec9c3b
MichaelHdz <hernandezmora07@gmail.com>
2023-03-25 05:06:12 -0500

    register
    no probado pero ajustado a como pidio los usuarios la casa matriz, aun faltan cosas del login, se termina mañana

---
+++ controllers/auth.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const db = mysql.createConnection({
+    host:process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+exports.register = (req, res) => {
+
+    const {name, email, password, passwordconfirm, address, phone, document} = req.body;
+    if (password!== passwordconfirm) {
+        return res.status(400).send("passwords do not match");
+    }
+
+    db.query("SELECT * FROM usuarios WHERE email =?", [email], async (err, resultados) => {
+        if (err) {
+            console.log(err);
+            
+        }
+        if (resultados.length > 0) {
+            return res.render('register', {
+                message: 'El email ya esta registrado'
+            });
+        } else if (password!== passwordconfirm) {
+            return res.render('register', {
+                message: 'las contraseñas no coinciden'
+            });
+        }
+        
+        let hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+
+
+    })
+    db.query("INSERT INTO usuarios SET ?", {name: name, email: email, password: hashedPassword, direccion: address, telefono: phone, documento: document}, (err, resultados) => {
+        if (err) {
+            console.log(err);
+        }else {
+            return res.redirect('login', {
+                message: 'Usuario registrado'
+            });
+        }
+    })
+
+
+}
