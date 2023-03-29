117b7540f55ad1e96790fd3a4f0d6331a10d6b3e
MichaelHdz <hernandezmora07@gmail.com>
2023-03-29 00:46:51 -0500

    cambios register/login

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const authcontroller = require('../controllers/auth');
 const router = express.Router();
 
 router.post('/register', authcontroller.register );
+router.post('/login', authcontroller.login );
+
 
 
 
