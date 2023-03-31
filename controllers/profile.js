7df8e0483c4b5214a7c622283bc46e923ca605be
MichaelHdz <hernandezmora07@gmail.com>
2023-03-31 05:26:28 -0500

    terminadas historias del sprint
    falta de frontend

--- controllers/profile.js
+++ controllers/profile.js
@@ -13,7 +13,7 @@ exports.profile = (req, res) => {
         if (err) {
             console.log(err);
         }
-        if (resultados.Length == 0) {
+        if (resultados==undefined) {
             return res.status(404).render('profile', {
                 message: 'No se encontró el usuario'
             });
@@ -23,4 +23,45 @@ exports.profile = (req, res) => {
         }
     });
 }
+
+exports.editProfile = (req, res) => {
+    db.query("SELECT * FROM usuarios WHERE idusuarios = ?", [req.session.userId], (err, resultados) => {
+      if (err) {
+        console.log(err);
+      }
+      if (resultados.length == 0) {
+        return res.status(404).render('editProfile', {
+          message: 'No se encontró el usuario'
+        });
+      } else {
+        const { us_nombre, us_correo, us_password, us_telefono, us_direccion, us_documento } = resultados[0];
+        res.render('editProfile', { us_nombre, us_correo, us_password, us_telefono, us_direccion, us_documento });
+      }
+    });
+  }
+
+  exports.updateProfile = (req, res) => {
+    // Obtener los datos del formulario
+    const { us_nombre, us_correo, us_password, us_passwordconfirm, us_direccion, us_telefono, us_documento } = req.body;
+    
+    if (us_password !== us_passwordconfirm) {
+      return res.render('editProfile', {
+        message: 'Las contraseñas no coinciden'
+      });
+    }
+    
+    // Encriptar la contraseña
+    bcrypt.hash(us_password, 10, (err, hashedPassword) => {
+      if (err) throw err;
+      
+      // Actualizar la información del usuario en la base de datos
+      const query = 'UPDATE usuarios SET ? WHERE idusuarios = ?';
+      db.query(query, [{us_nombre, us_correo, us_password: hashedPassword, us_direccion, us_telefono, us_documento}, req.session.userId], (error, results) => {
+        if (error) throw error;
+        
+        // Redirigir al usuario a la página de perfil
+        res.redirect('/profile');
+      });
+    });
+  }
 
